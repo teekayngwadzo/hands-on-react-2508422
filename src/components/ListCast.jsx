85e41f6ee0aebebc1c11fd1ea21f681e88a87807
ngwadzo <48917605+teekayngwadzo@users.noreply.github.com>
2023-01-18 07:40:34 +0000

    refactoring code

--- src/components/ListCast.jsx
+++ src/components/ListCast.jsx
@@ -1,16 +1,5 @@
-import { useState, useEffect } from "react";
 
-export default ({onChoice}) => {
-  const [cast, setCast] = useState([]);
-
-  async function fetchCast() {
-    const response = await fetch('cast.json');
-    setCast(await response.json());
-  }
-
-  useEffect(() => {
-    fetchCast();
-  });
+export default ({cast,onChoice}) => {
 
   return (
     <div style={{
