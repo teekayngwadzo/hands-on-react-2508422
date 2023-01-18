d0fbbae4c6c8d552217344b989addde58cfd8e5d
ngwadzo <48917605+teekayngwadzo@users.noreply.github.com>
2023-01-18 05:26:49 +0000

    useEffect and useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
+import React, { Component } from "react";
+import Welcome from "./components/Welcome";
+import Support from "./components/Support";
+import ListCast from "./components/ListCast";
 
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
   return (
     <div className="container">
       <article>
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
           <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <p>
+            Members of an <b>intergalactic alliance</b>
+            <br />
+            paving the way for peace and benevolence among all species. They are
+            known for their enthusiasm for science, for their love of fun, and
+            their dedication to education.
+          </p>
+          <ListCast />
           <Support />
         </hgroup>
       </article>
     </div>
-  )
+  );
 }
-export default App
+export default App;
